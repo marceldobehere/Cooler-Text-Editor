857f00b8a1386acb3855176d306d5a286b5f46c6
Masl <marcel.z2006@gmail.com>
2023-05-29 19:21:13 +0200

    a

--- Cooler Text Editor/Program.cs
+++ Cooler Text Editor/Program.cs
@@ -15,8 +15,8 @@ public class Program
 
     public static void Main()
     {
-        Console.OutputEncoding = System.Text.Encoding.Unicode;
-        Console.InputEncoding = System.Text.Encoding.Unicode;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
         Console.BackgroundColor = ConsoleColor.Black;
         Console.ForegroundColor = ConsoleColor.White;
         Console.Clear();
