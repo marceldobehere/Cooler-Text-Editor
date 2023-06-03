e626f7b1a384d624ce408a51bd142f91efa6c97a
Masl <marcel.z2006@gmail.com>
2023-06-03 20:32:56 +0200

    a

---
+++ Cooler Text Editor/SyntaxStuff/TreeSitterTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cooler_Text_Editor.SyntaxStuff
+{
+    public class TreeSitterTest
+    {
+
+    }
+}
